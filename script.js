ad81c7a9dcf43a7bdc12b94080fbc7854d854bd5
Caiohls <99880046+Caiohls@users.noreply.github.com>
2024-10-11 17:57:49 -0300

    versao0.06

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 window.addEventListener('load', function() {
     alert('Bem-vindo à nossa história de amor! Pabline e Caio!');
 });
+
+let slideIndex = 0;
+const slides = document.querySelector('.gallery');
+const totalSlides = slides.children.length;
+const visibleSlides = 3; // Quantidade de imagens visíveis ao mesmo tempo
+
+function mostrarSlide(index) {
+    // Garantir que o índice está dentro dos limites
+    if (index > totalSlides - visibleSlides) {
+        slideIndex = totalSlides - visibleSlides; // Não ir além das últimas imagens visíveis
+    } else if (index < 0) {
+        slideIndex = 0;
+    }
+
+    // Calcular a largura de cada slide em porcentagem
+    const slideWidthPercentage = 100 / visibleSlides / 2;
+
+    // Calcular a distância para mover com base na largura de cada slide
+    const offset = -slideIndex * slideWidthPercentage;
+    slides.style.transform = `translateX(${offset}%)`;
+
+}
+
+function mudarSlide(n) {
+    slideIndex += n;
+    mostrarSlide(slideIndex);
+}
+
+// Exibe o primeiro slide ao carregar a página
+mostrarSlide(slideIndex);
